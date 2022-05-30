654bd2f44276545121d7c99d0b3c9e8a22e5d3e2
Taylor Harrison Carroll <54039744+taylorhcarroll@users.noreply.github.com>
2022-05-29 19:50:55 -0500

    removed img creation from init func in projectile

--- source/projectile.lua
+++ source/projectile.lua
@@ -3,13 +3,15 @@ local gfx <const> = pd.graphics
 
 class('Projectile').extends(gfx.sprite)
 
+-- this chunk sets up the image for the basic bullet, moved out of the init func so as to not redraw it every time it's called
+projectileSize = 4
+projectileImage = gfx.image.new(projectileSize * 2, projectileSize * 2)
+-- allows us to draw directly on an image instead of the screen
+gfx.pushContext(projectileImage)
+    gfx.drawCircleAtPoint(projectileSize, projectileSize, projectileSize)
+gfx.popContext()
+
 function Projectile:init(x, y, speed)
-    local projectileSize = 4
-    local projectileImage = gfx.image.new(projectileSize * 2, projectileSize * 2)
-    -- allows us to draw directly on an image instead of the screen
-    gfx.pushContext(projectileImage)
-        gfx.drawCircleAtPoint(projectileSize, projectileSize, projectileSize)
-    gfx.popContext()
     self:setImage(projectileImage)
     -- this property added to use moveWithCollisions
     self:setCollideRect(0, 0, self:getSize())
